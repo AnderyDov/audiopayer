bd12dc4afe90de3219806f8fd513ce685bf82f86
Andrey <dovban.andreyy@rambler.ru>
2023-03-12 15:15:00 +0300

    стилизация ползунка

--- player.js
+++ player.js
@@ -7,58 +7,43 @@ let prev = document.querySelector('.prev');
 let next = document.querySelector('.next');
 
 // Массив с названиями песен
-let playlist = ['treck1.mp3', 'treck2.mp3'];
+let playlist = ['treck2.mp3', 'treck2.mp3'];
 
-let treck; // Переменная с индексом трека
-
-// Событие перед загрузкой страницы
-window.onload = function () {
-    treck = 0; // Присваиваем переменной ноль
+play.onclick = (e) => {
+    const t = e.currentTarget;
+    t.firstElementChild.classList.toggle('hide');
+    t.lastElementChild.classList.toggle('hide');
+    audio.src = './audio/' + playlist[0];
+    if (
+        t.firstElementChild.classList.contains('hide') &&
+        !t.lastElementChild.classList.contains('hide')
+    ) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
 };
 
-function switchTreck(numTreck) {
-    audio.src = './audio/' + playlist[numTreck];
-    audio.currentTime = 0;
-    audio.play();
-}
+// pause.addEventListener('click', function () {
+//     // clearInterval(audioPlay);
+// });
 
-play.addEventListener('click', function () {
-    audio.play();
-    audioPlay = setInterval(function () {
-        let audioTime = Math.round(audio.currentTime);
-        let audioLength = Math.round(audio.duration);
-        time.style.width = (audioTime * 100) / audioLength + '%';
-        if (audioTime == audioLength && treck < 2) {
-            treck++;
-            switchTreck(treck);
-        } else if (audioTime == audioLength && treck >= 2) {
-            treck = 0;
-            switchTreck(treck);
-        }
-    }, 10);
-});
+// prev.addEventListener('click', function () {
+//     if (treck > 0) {
+//         treck--;
+//         switchTreck(treck);
+//     } else {
+//         treck = 3;
+//         switchTreck(treck);
+//     }
+// });
 
-pause.addEventListener('click', function () {
-    audio.pause();
-    clearInterval(audioPlay);
-});
-
-prev.addEventListener('click', function () {
-    if (treck > 0) {
-        treck--;
-        switchTreck(treck);
-    } else {
-        treck = 3;
-        switchTreck(treck);
-    }
-});
-
-next.addEventListener('click', function () {
-    if (treck < 3) {
-        treck++;
-        switchTreck(treck);
-    } else {
-        treck = 0;
-        switchTreck(treck);
-    }
-});
+// next.addEventListener('click', function () {
+//     if (treck < 3) {
+//         treck++;
+//         switchTreck(treck);
+//     } else {
+//         treck = 0;
+//         switchTreck(treck);
+//     }
+// });
